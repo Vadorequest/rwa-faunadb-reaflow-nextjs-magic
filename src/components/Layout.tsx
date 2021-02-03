42a44e93e6ea21db9c90269fd01bddefe91691cf
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-02-03 12:22:56 +0100

    Remove all demo code, keep only the minimum stuff

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,33 +7,27 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = 'POC Next.js + Reaflow' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
+
     <header>
       <nav>
         <Link href="/">
           <a>Home</a>
         </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
       </nav>
     </header>
+
     {children}
+
     <footer>
       <hr />
-      <span>I'm here to stay (Footer)</span>
+      <span>Made with Next.js and <a href={'https://github.com/reaviz/reaflow'}>Reaflow</a></span>
     </footer>
   </div>
 )
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout>
+
+  </Layout>
+);
+
+export default IndexPage;
