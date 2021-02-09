277ebc9aa7f08cb77a9bc23bf34d63cbc11b6e16
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-02-09 11:30:14 +0100

    Add BasePortData

---
+++ src/types/BasePortData.ts
@@ -0,0 +1,11 @@
+import { PortData } from 'reaflow';
+
+/**
+ * Shape of any port.
+ * Extends the Reaflow.PortData.
+ *
+ * Doesn't do anything useful at the moment, used in case we'd need to extend it later on.
+ */
+export type BasePortData = PortData & {};
+
+export default BasePortData;
