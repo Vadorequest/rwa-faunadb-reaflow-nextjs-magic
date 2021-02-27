4a216eae00b38932b82eb24f0eca3b0693dba50e
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-02-27 10:22:21 +0100

    Use recoil-devtools-log-monitor - Crashes the app (see https://github.com/ulises-jeremias/recoil-devtools/issues/5)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import {
 import { Router } from 'next/router';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import LogMonitor from 'recoil-devtools-log-monitor';
 import { RecoilExternalStatePortal } from '../components/RecoilExternalStatePortal';
 import '../utils/fontAwesome';
 
@@ -31,6 +32,7 @@ const App: React.FunctionComponent<Props> = (props): JSX.Element => {
   return (
     <ChakraProvider>
       <RecoilRoot>
+        <LogMonitor />
         <Component {...pageProps} />
         <RecoilExternalStatePortal />
       </RecoilRoot>
