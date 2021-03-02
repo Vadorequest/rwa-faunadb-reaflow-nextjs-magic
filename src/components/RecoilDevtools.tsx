e3b109cae4baa7ee35230a07f8f2aa723c339284
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-02 10:25:40 +0100

    More doc

--- src/components/RecoilDevtools.tsx
+++ src/components/RecoilDevtools.tsx
@@ -4,16 +4,28 @@ import LogMonitor from 'recoil-devtools-log-monitor';
 import { RecoilLogger } from 'recoil-devtools-logger';
 import DisplayOnBrowserMount from './DisplayOnBrowserMount';
 
-export const RecoilDevtools = () => (
-  <DisplayOnBrowserMount>
-    <RecoilLogger />
-    <DockMonitor
-      toggleVisibilityKey="ctrl-h"
-      changePositionKey="ctrl-q"
-      changeMonitorKey="ctrl-m"
-      defaultIsVisible
-    >
-      <LogMonitor />
-    </DockMonitor>
-  </DisplayOnBrowserMount>
-);
+/**
+ * Adds Recoil dev tools for easier understanding and debug of what happens in the Recoil store.
+ * Enabled in development mode only.
+ *
+ * @see https://github.com/ulises-jeremias/recoil-devtools
+ */
+export const RecoilDevtools = () => {
+  if (process.env.NODE_ENV !== 'development') {
+    return null;
+  }
+
+  return (
+    <DisplayOnBrowserMount>
+      <RecoilLogger />
+      <DockMonitor
+        toggleVisibilityKey="ctrl-h"
+        changePositionKey="ctrl-q"
+        changeMonitorKey="ctrl-m" // Unnecessary for our app, because we only have one Recoil store
+        defaultIsVisible
+      >
+        <LogMonitor />
+      </DockMonitor>
+    </DisplayOnBrowserMount>
+  );
+};
