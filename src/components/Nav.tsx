aa78d1fa9eafa1986ec4812fe39eb54bc8ae6286
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-08 18:51:22 +0100

    Add skeleton UI for Nav/Footer

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,11 @@
+import { QuestionOutlineIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Button,
+  Flex,
+  Spacer,
+  Tooltip,
+} from '@chakra-ui/react';
 import { css } from '@emotion/react';
 import React from 'react';
 import settings from '../settings';
@@ -10,20 +18,35 @@ const Nav: React.FunctionComponent<Props> = (props) => {
       css={css`
         .nav {
           height: ${settings.layout.nav.height}px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
 
           a {
             margin-left: 10px;
             margin-right: 10px;
           }
         }
-
       `}
     >
       <nav className={'nav'}>
+        <Flex>
+          <Box p="2">
+            You are currently <b>anonymous</b> and working on a shared document.
+          </Box>
 
+          <Spacer />
+
+          <Box p="2">
+            <Button variant={'primary'}>
+              Log in
+            </Button>
+            <Button variant={'primary'}>
+              Create account
+              &nbsp;
+              <Tooltip label="Creating an account will allow you to work on your own document" fontSize="md">
+                <QuestionOutlineIcon />
+              </Tooltip>
+            </Button>
+          </Box>
+        </Flex>
       </nav>
     </header>
   );
