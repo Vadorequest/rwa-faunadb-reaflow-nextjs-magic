80a67ac2fd2d112794e478865a912f253607912a
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-09 16:10:50 +0100

    Make label displays below the edge line

--- src/components/edges/Label.tsx
+++ src/components/edges/Label.tsx
@@ -12,15 +12,16 @@ const Label: FC<Partial<LabelProps>> = (props) => {
     originalText,
   } = props;
 
-  // Dynamically resolve a right padding based on the size of the text to display (moves the text to the left to center it)
-  let paddingRightX = (text?.length || 0) * 2; // Padding left 2px per character
+  const offsetY = -20; // Make label displays below the edge line
+  // Dynamically resolve a right offset based on the size of the text to display (moves the text to the left to center it)
+  let offsetX = (text?.length || 0) * 2; // Padding left 2px per character
 
-  if (paddingRightX > 50) {
-    paddingRightX = 50; // Mustn't be higher than 50, otherwise it'll be floating above another node
+  if (offsetX > 50) {
+    offsetX = 50; // Mustn't be higher than 50, otherwise it'll be floating above another node
   }
 
   return (
-    <g transform={`translate(${(x || 0) - paddingRightX}, ${y})`}>
+    <g transform={`translate(${(x || 0) - offsetX}, ${(y || 0) - offsetY})`}>
       <title>{originalText}</title>
       <text
         className={classNames('label', className)}
