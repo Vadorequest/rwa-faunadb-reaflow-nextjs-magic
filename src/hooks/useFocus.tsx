006231989b71ad0815af673a3c7268472be9764f
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-09 17:33:57 +0100

    Doc

--- src/hooks/useFocus.tsx
+++ src/hooks/useFocus.tsx
@@ -3,6 +3,15 @@ import {
   useRef,
 } from 'react';
 
+/**
+ * Hook to help control input focus dynamically.
+ *
+ * @example
+ *  const [inputRef, setInputFocus] = useFocus()
+ *  <input ref={inputRef} />
+ *
+ * @see https://stackoverflow.com/a/54159564/2391795
+ */
 const useFocus = (): [any, () => void] => {
   const htmlElRef: MutableRefObject<any> = useRef(null);
   const setFocus = (): void => {
