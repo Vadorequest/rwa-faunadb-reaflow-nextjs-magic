9bbd7778aea4622ec69c560827d5e59ae3616a5c
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-10 11:53:48 +0100

    Rename TOKEN_SECRET > CRYPTO_TOKEN_SECRET

--- src/lib/auth/crypto.ts
+++ src/lib/auth/crypto.ts
@@ -1,9 +1,9 @@
 import Iron from '@hapi/iron'
 
-const TOKEN_SECRET = process.env.TOKEN_SECRET as string;
+const CRYPTO_TOKEN_SECRET = process.env.CRYPTO_TOKEN_SECRET as string;
 
-if (!TOKEN_SECRET || TOKEN_SECRET?.length < 32) {
-  throw new Error(`You must define a "TOKEN_SECRET" environment variable of at least 32 characters in order to use authentication. Found "${TOKEN_SECRET}".`);
+if (!CRYPTO_TOKEN_SECRET || CRYPTO_TOKEN_SECRET?.length < 32) {
+  throw new Error(`You must define a "CRYPTO_TOKEN_SECRET" environment variable of at least 32 characters in order to use authentication. Found "${CRYPTO_TOKEN_SECRET}".`);
 }
 
 /**
@@ -11,11 +11,11 @@ if (!TOKEN_SECRET || TOKEN_SECRET?.length < 32) {
  *
  * @param data
  */
-export const encryptData = async <Data>(data: Data): Promise<string> => Iron.seal(data, TOKEN_SECRET, Iron.defaults);
+export const encryptData = async <Data>(data: Data): Promise<string> => Iron.seal(data, CRYPTO_TOKEN_SECRET, Iron.defaults);
 
 /**
  * Decrypts a token and returns the data contains within.
  *
  * @param token
  */
-export const decryptToken = async <Data>(token: string): Promise<Data> => Iron.unseal(token, TOKEN_SECRET, Iron.defaults);
+export const decryptToken = async <Data>(token: string): Promise<Data> => Iron.unseal(token, CRYPTO_TOKEN_SECRET, Iron.defaults);
