c1a196aaf08489c05602f2077e094642004248fd
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-10 13:09:14 +0100

    Doc

--- src/lib/faunadb/models/userModel.ts
+++ src/lib/faunadb/models/userModel.ts
@@ -8,18 +8,37 @@ import {
 } from '../faunadb';
 
 export class UserModel {
+  /**
+   * Creates a new user in the "users" collection.
+   *
+   * @param email
+   */
   async createUser(email: string): Promise<User | undefined> {
     return adminClient.query<User>(q.Create(q.Collection('users'), {
       data: { email },
     }));
   }
 
+  /**
+   * Find a user using the "users_by_email" index.
+   *
+   * @param email
+   */
   async getUserByEmail(email: string): Promise<User | undefined> {
     return adminClient.query<User>(
       q.Get(q.Match(q.Index('users_by_email'), email)),
     ).catch(() => undefined);
   }
 
+  /**
+   * Generates a FaunaDB Token for the user.
+   *
+   * The token is associated to the user instance.
+   *
+   * @param user
+   *
+   * @see https://docs.fauna.com/fauna/current/api/fql/functions/tokens?lang=javascript
+   */
   async obtainFaunaDBToken(user: User): Promise<string | undefined> {
     return adminClient.query<FaunadbToken>(
       q.Create(q.Tokens(), { instance: q.Select('ref', user) }),
--- src/types/faunadb/FaunadbToken.ts
+++ src/types/faunadb/FaunadbToken.ts
@@ -2,7 +2,13 @@ import { values } from 'faunadb';
 import { FaunadbBaseFields } from './FaunadbBaseFields';
 import Ref = values.Ref;
 
+/**
+ * A Token created by FaunaDB.
+ *
+ * @see https://docs.fauna.com/fauna/current/api/fql/functions/tokens?lang=javascript
+ */
 export type FaunadbToken = FaunadbBaseFields & {
   instance: Ref;
   secret: string;
+  data?: any; // Optional, consider those as token metadata (not user's metadata)
 }
