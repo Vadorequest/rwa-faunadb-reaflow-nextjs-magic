cbdb97b457b42a5263bea37ae619af7673fd634d
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-10 10:18:08 +0100

    Refactoring magicClient and make sure neither crash when called from an unexpected runtime

--- src/lib/magicAdmin.ts
+++ src/lib/magicAdmin.ts
@@ -1,3 +1,12 @@
+import { isBrowser } from '@unly/utils';
 const { Magic } = require('@magic-sdk/admin');
 
-export const magicAdmin = new Magic(process.env.MAGIC_SECRET_KEY);
+/**
+ * Initialize a Magic Link admin instance (on the server).
+ *
+ * Acts as a singleton.
+ * Won't crash if called on the browser (universal).
+ *
+ * @see https://docs.magic.link/admin-sdk/node/get-started#installation
+ */
+export const magicAdmin = !isBrowser() ? new Magic(process.env.MAGIC_SECRET_KEY) : null;
