96f1e23c5dd0b5bbf48bd32cb769453d85868cc6
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-11 23:46:17 +0100

    BREAKING - Add EndNode + automatically create start/end nodes if any of them is missing (will destroy all existing datasets upon load)

---
+++ src/components/nodes/EndNode.tsx
@@ -0,0 +1,85 @@
+import { css } from '@emotion/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Fragment } from 'react';
+import settings from '../../settings';
+import BaseNodeComponent from '../../types/BaseNodeComponent';
+import { BaseNodeDefaultProps } from '../../types/BaseNodeDefaultProps';
+import BaseNodeProps from '../../types/BaseNodeProps';
+import BasePortData from '../../types/BasePortData';
+import { SpecializedNodeProps } from '../../types/nodes/SpecializedNodeProps';
+import { StartNodeData } from '../../types/nodes/StartNodeData';
+import NodeType from '../../types/NodeType';
+import { createPort } from '../../utils/ports';
+import BaseNode from './BaseNode';
+
+type Props = {} & BaseNodeProps<StartNodeData>;
+
+const nodeType: NodeType = 'end';
+const defaultWidth = 100;
+const defaultHeight = 100;
+
+/**
+ * End node.
+ *
+ * Used to define the workflow's finish line.
+ * Is unique, there can be only one EndNode, and it's required and must always be present.
+ *
+ * Displays an end icon.
+ * Has only one west port.
+ */
+const EndNode: BaseNodeComponent<Props> = (props) => {
+  return (
+    <BaseNode
+      hasCloneAction={false}
+      hasDeleteAction={false}
+      {...props}
+    >
+      {
+        (nodeProps: SpecializedNodeProps<StartNodeData>) => {
+          return (
+            <Fragment>
+              <div
+                className={`node-content ${nodeType}-content`}
+                css={css`
+                  svg {
+                    margin: 10px;
+                    margin-left: 15px;
+                    color: red;
+                  }
+                `}
+              >
+                <FontAwesomeIcon
+                  icon={['fas', 'flag-checkered']}
+                  size={'4x'}
+                />
+              </div>
+            </Fragment>
+          );
+        }
+      }
+    </BaseNode>
+  );
+};
+
+EndNode.getDefaultPorts = (): BasePortData[] => {
+  return [
+    createPort({
+      height: settings.canvas.ports.radius,
+      width: settings.canvas.ports.radius,
+      alignment: 'CENTER',
+      side: 'WEST',
+    }),
+  ];
+};
+
+EndNode.getDefaultNodeProps = (): BaseNodeDefaultProps => {
+  return {
+    type: nodeType,
+    defaultWidth: defaultWidth,
+    defaultHeight: defaultHeight,
+    // @ts-ignore
+    ports: EndNode.getDefaultPorts(),
+  };
+};
+
+export default EndNode;
