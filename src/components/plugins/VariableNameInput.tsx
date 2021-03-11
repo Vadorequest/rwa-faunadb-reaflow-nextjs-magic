56d3a17cb7a6a4fe470b17287ce19576bc03aa9c
Dan-Hen <leo.clot16@gmail.com>
2021-03-12 00:12:56 +0100

    bounce animation

--- src/components/plugins/VariableNameInput.tsx
+++ src/components/plugins/VariableNameInput.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classnames from 'classnames';
 import React, {
   PropsWithChildren,
   useState,
@@ -78,6 +79,7 @@ export const VariableNameInput = <NodeData extends NodeDataWithVariableName = No
           color: #6E6E6E;
           margin-left: 10px;
           cursor: pointer;
+          animation: ${isModified ? 'bounce ease 1s infinite' : ''};
         }
 
         text {
@@ -100,6 +102,19 @@ export const VariableNameInput = <NodeData extends NodeDataWithVariableName = No
           100% {
             opacity: 0;
           }
+        }
+          
+        @keyframes bounce {
+          0% {
+            transform:scale(1.1);
+          }
+          50% {
+            transform: scale(0.85);
+          }
+          100% {
+            transform: scale(1.1);
+          }
+        }          
       `}
     >
       <input
