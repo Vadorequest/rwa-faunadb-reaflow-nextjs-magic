8bef36925757ced239f49d9fa7fd615ab7a8ea3c
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-11 15:14:24 +0100

    Add SelectAll utility

--- src/utils/fql.ts
+++ src/utils/fql.ts
@@ -1,4 +1,13 @@
-import { ExprArg } from 'faunadb';
+import {
+  Collection,
+  Documents,
+  ExprArg,
+  Get,
+  Lambda,
+  Map,
+  Paginate,
+  Var,
+} from 'faunadb';
 import { q } from '../lib/faunadb/faunadb';
 
 const { Exists, If, Delete, Update, CreateFunction, CreateRole, Role } = q;
@@ -19,4 +28,18 @@ export const CreateOrUpdateRole = (obj: any) => If(
   Exists(Role(obj.name)),
   Update(Role(obj.name), { membership: obj.membership, privileges: obj.privileges }),
   CreateRole(obj),
+);
+
+/**
+ *
+ * @param collectionName
+ *
+ * @see https://fauna.com/blog/modernizing-from-postgresql-to-serverless-with-fauna-part-1#select
+ */
+export const SelectAll = (collectionName: string) => Map(
+  Paginate(Documents(Collection(collectionName))),
+  Lambda(
+    ['ref'],
+    Get(Var('ref'))
+  ),
 );
