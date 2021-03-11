ff835173ce997bd754511c4f6407c213fc2c7f23
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-11 21:44:37 +0100

    Add global styles + Animated3Dots.tsx component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import {
 import { Router } from 'next/router';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import GlobalStyles from '../components/GlobalStyles';
 import { RecoilDevtools } from '../components/RecoilDevtools';
 import { RecoilExternalStatePortal } from '../components/RecoilExternalStatePortal';
 import '../utils/fontAwesome';
@@ -38,6 +39,7 @@ const App: React.FunctionComponent<Props> = (props): JSX.Element => {
         {/* Utility component allowing to use the Recoil state outside of a React component */}
         <RecoilExternalStatePortal />
 
+        <GlobalStyles />
         <Component {...pageProps} />
       </RecoilRoot>
     </ChakraProvider>
