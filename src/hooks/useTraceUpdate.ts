e22861975c5391a1e2c9264ac4d82af353a3258b
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-12 19:07:21 +0100

    Add useTraceUpdate.ts hook

---
+++ src/hooks/useTraceUpdate.ts
@@ -0,0 +1,44 @@
+import {
+  useEffect,
+  useRef,
+} from 'react';
+
+/**
+ * Helps tracking the props changes made in a react functional component.
+ *
+ * Prints the name of the properties/states variables causing a render (or re-render).
+ * For debugging purposes only.
+ *
+ * @usage You can simply track the props of the components like this:
+ *  useRenderingTrace('MyComponent', props);
+ *
+ * @usage You can also track additional state like this:
+ *  const [someState] = useState(null);
+ *  useRenderingTrace('MyComponent', { ...props, someState });
+ *
+ * @param componentName Name of the component to display
+ * @param propsAndStates
+ * @param level
+ *
+ * @see https://stackoverflow.com/a/51082563/2391795
+ */
+const useRenderingTrace = (componentName: string, propsAndStates: any, level: 'debug' | 'info' | 'log' = 'debug') => {
+  const prev = useRef(propsAndStates);
+
+  useEffect(() => {
+    const changedProps = Object.entries(propsAndStates).reduce((property: any, [key, value]: [string, any]) => {
+      if (prev.current[key] !== value) {
+        property[key] = [prev.current[key], value];
+      }
+      return property;
+    }, {});
+
+    if (Object.keys(changedProps).length > 0) {
+      console[level](`[${componentName}] Changed props:`, changedProps);
+    }
+
+    prev.current = propsAndStates;
+  });
+};
+
+export default useRenderingTrace;
