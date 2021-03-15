e162254b2ae73014ac6b947160a18b78c1c67672
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-15 14:04:43 +0100

    Debounce onHeightChange through Textarea component

--- src/components/plugins/Textarea.tsx
+++ src/components/plugins/Textarea.tsx
@@ -1,6 +1,8 @@
 import { css } from '@emotion/react';
 import React from 'react';
 import TextareaAutosize, { TextareaAutosizeProps } from 'react-textarea-autosize';
+import { TextareaHeightChangeMeta } from 'react-textarea-autosize/dist/declarations/src';
+import { useDebouncedCallback } from 'use-debounce';
 
 type Props = {} & TextareaAutosizeProps;
 
@@ -16,14 +18,29 @@ export const Textarea: React.FunctionComponent<Props> = (props) => {
   const {
     minRows = 1,
     maxRows = 5,
+    onHeightChange,
     ...rest
   } = props;
+
+  /**
+   * Debounces notifications sent when the input's height changes.
+   *
+   * The input's height is dynamic and depends on the content.
+   * It is debounced to avoid sending too much updates in a burst when jumping several lines at once.
+   */
+  const debouncedOnHeightChange = useDebouncedCallback(
+    (height: number, meta: TextareaHeightChangeMeta) => {
+      onHeightChange?.(height, meta);
+    },
+    1000, // Wait 100ms for other changes to happen, if no change happen then invoke the update
+  );
 
   return (
     <TextareaAutosize
       minRows={minRows}
       maxRows={maxRows}
       {...rest}
+      onHeightChange={debouncedOnHeightChange}
       css={css`
         resize: none;
         width: 100%;
