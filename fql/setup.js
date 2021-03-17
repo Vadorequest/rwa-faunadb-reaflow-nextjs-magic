652af82ec6e915956384524c81edcc35b00e21b4
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-17 21:17:19 +0100

    Fix Editor role create/write permissions + add doc

--- fql/setup.js
+++ fql/setup.js
@@ -1,10 +1,10 @@
-// Step 1: Create a "Users" collection
+// ---------------------- Step 1: Create a "Users" collection ----------------------
 CreateCollection({ name: 'Users' });
 
-// Step 2: Create "Canvas" collection
+// ---------------------- Step 2: Create "Canvas" collection ----------------------
 CreateCollection({ name: 'Canvas' });
 
-// Step 3: Create indexes
+// ---------------------- Step 3: Create indexes ----------------------
 // Index to filter users by email
 // Necessary for authentication, to find the user document based on their email
 CreateIndex({
@@ -33,7 +33,13 @@ CreateIndex({
   ],
 });
 
-// Step 4: Create roles
+// ---------------------- Step 4: Create roles ----------------------
+
+// The "Editor" role is assigned to all authenticated users
+// It is automatically assigned when a user is authenticated, because it defines "membership" to the Users collection
+// It is secure because the token is generated upon login on the server-side and stored in a "httpOnly" cookie that can only be read/written on the server-side
+// The token is specific to the user and is used on the frontend
+// The token only allows the user to read/write documents that belongs to him
 CreateRole({
   name: 'Editor',
   // All users should be editors (will apply to authenticated users only).
@@ -51,7 +57,7 @@ CreateRole({
     {
       resource: Collection('Canvas'),
       actions: {
-        // Editors should be able to read (+ history) only Canvas documents that belongs to them.
+        // Editors should be able to read (+ history) of Canvas documents that belongs to them.
         read: Query(
           Lambda('ref', Equals(
             CurrentIdentity(),
@@ -64,15 +70,36 @@ CreateRole({
             Select(['data', 'owner'], Get(Var('ref'))),
           )),
         ),
-        // Editors should be able to edit only Canvas documents that belongs to them (but I don't know how to write that).
-        write: true,
-        // Editors should be able to create only Canvas documents that belongs to them (but I don't know how to write that).
-        create: true,
+        // Editors should be able to edit only Canvas documents that belongs to them
+        write: Lambda(
+          ["oldData", "newData", "ref"],
+          And(
+            // The owner in the current data (before writing them) must be the current user
+            Equals(
+              CurrentIdentity(),
+              Select(["data", "owner"], Var("oldData"))
+            ),
+            // The owner must not change
+            Equals(
+              Select(["data", "owner"], Var("oldData")),
+              Select(["data", "owner"], Var("newData"))
+            )
+          )
+        ),
+        // Editors should be able to create only Canvas documents that belongs to them
+        create: Lambda("values", Equals(
+          CurrentIdentity(),
+          Select(["data", "owner"], Var("values")))
+        ),
       },
     },
   ],
 });
 
+// The "Public" role is assigned to anyone who isn't authenticated
+// It doesn't use "membership" (unlike "Editor" role) but a token created manually that doesn't expire
+// It is secure because the token only grant access to the special document of id "1", which is shared amongst all guests
+// Guests can only read/write this particular document and not any other
 CreateRole({
   name: 'Public',
   // The public role is meant to be used to generate a token which allows anyone (unauthenticated users) to update the canvas
@@ -91,7 +118,7 @@ CreateRole({
             ),
           ),
         ),
-        // Guests should only be allowed to update the Canvas of id "1" (but I don't know how to write that)
+        // Guests should only be allowed to update the Canvas of id "1"
         write: Lambda(
           ['oldData', 'newData', 'ref'],
           Equals(
@@ -99,14 +126,15 @@ CreateRole({
             Select(['id'], Var('ref')),
           ),
         ),
-        // Guests should only be allowed to create the Canvas of id "1", but this requires admin permissions and will fail
-        // See https://fauna-community.slack.com/archives/CAKNYCHCM/p1615413941454700
+        // Guests should only be allowed to create the Canvas of id "1"
         create: Lambda('values',
           Equals(
             '1',
             Select(['ref', 'id'], Var('values')),
           ),
         ),
+        // Creating a record with a custom ID requires history_write privilege
+        // See https://fauna-community.slack.com/archives/CAKNYCHCM/p1615413941454700
         history_write: Lambda(
           ['ref', 'ts', 'action', 'data'],
           Equals(
