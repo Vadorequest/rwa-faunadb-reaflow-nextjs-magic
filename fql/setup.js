16b55c876582c98763e547cf99724c018c892b05
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-18 22:22:08 +0100

    Misc

--- fql/setup.js
+++ fql/setup.js
@@ -22,7 +22,9 @@ CreateIndex({
   name: 'canvas_by_owner',
   source: Collection('Canvas'),
   // Needs permission to read the Users, because "owner" is specified in the "terms" and is a Ref to the "Users" collection
-  permissions: { read: Collection('Users') },
+  permissions: {
+    read: Collection('Users')
+  },
   // Allow to filter by owner ("Users")
   terms: [
     { field: ['data', 'owner'] },
