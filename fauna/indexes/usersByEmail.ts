062db26d30e33e0406a187b6d9cbc507d077468d
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-27 13:05:30 +0100

    Misc doc

--- fauna/indexes/usersByEmail.ts
+++ fauna/indexes/usersByEmail.ts
@@ -2,9 +2,10 @@ import { IndexResource } from 'fauna-gql-upload';
 import { Collection } from 'faunadb';
 
 /**
- * Index to filter users by email
+ * Index to filter users by email.
  *
  * Necessary for authentication, to find the user document based on their email.
+ * Unique index, will only return at most one user per email.
  */
 const usersByEmail: IndexResource = {
   name: 'usersByEmail',
