c2afd95574a8b00d73001c4d39635ce5eef64950
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-27 19:33:46 +0100

    Add buttons to edit/create projects (they don't do anything yet)

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,13 +3,19 @@ import {
   Box,
   Button,
   Flex,
+  Input,
   Spacer,
   Tooltip,
 } from '@chakra-ui/react';
 import { css } from '@emotion/react';
-import React, { Fragment } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, {
+  Fragment,
+  useState,
+} from 'react';
 import { useUserSession } from '../hooks/useUserSession';
 import settings from '../settings';
+import { Project } from '../types/graphql/graphql';
 import { humanizeEmail } from '../utils/user';
 import AuthFormModal from './AuthFormModal';
 
@@ -17,7 +23,9 @@ type Props = {}
 
 const Nav: React.FunctionComponent<Props> = (props) => {
   const user = useUserSession();
+  const [projectFormMode, setProjectFormMode] = useState<'display' | 'edit' | 'create'>('display');
   console.log('user', user);
+  const activeProject: Project | undefined = user?.projects?.[0];
 
   return (
     <header
@@ -57,9 +65,64 @@ const Nav: React.FunctionComponent<Props> = (props) => {
           <Box p="2">
             {
               user?.isAuthenticated ? (
-                <Fragment>
-                  Project: {user?.projects?.[0]?.label}
-                </Fragment>
+                <Flex>
+                  {
+                    projectFormMode === 'edit' && (
+                      <Input
+                        defaultValue={activeProject?.label}
+                        placeholder={'Project name'}
+                        onKeyPress={(e) => {
+                          if (e.code === 'Enter') {
+                            // Save the new project label
+                            setProjectFormMode('display');
+                          }
+                        }}
+                        autoFocus={true}
+                      />
+                    )
+                  }
+
+                  {
+                    projectFormMode === 'create' && (
+                      <Input
+                        placeholder={'New project name'}
+                        onKeyPress={(e) => {
+                          if (e.code === 'Enter') {
+                            // Create the new project
+                            setProjectFormMode('display');
+                          }
+                        }}
+                        autoFocus={true}
+                      />
+                    )
+                  }
+
+                  {
+                    projectFormMode === 'display' && (
+                      <b>{activeProject?.label}</b>
+                    )
+                  }
+
+                  <Tooltip label={`Edit title`}>
+                    <Button
+                      size={'xs'}
+                      variant={'ghost'}
+                      onClick={() => setProjectFormMode('edit')}
+                    >
+                      <FontAwesomeIcon icon={'edit'} />
+                    </Button>
+                  </Tooltip>
+
+                  <Tooltip label={`Add a new project`}>
+                    <Button
+                      size={'xs'}
+                      variant={'ghost'}
+                      onClick={() => setProjectFormMode('create')}
+                    >
+                      <FontAwesomeIcon icon={'plus-circle'} />
+                    </Button>
+                  </Tooltip>
+                </Flex>
               ) : (
                 null
               )
