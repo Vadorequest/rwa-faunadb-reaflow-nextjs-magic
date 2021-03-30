f16841dca6ad1ef2d31e7e3f836e943331f3aa86
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2021-03-30 09:55:48 +0200

    Update generated GQL schema and types

--- src/types/graphql/graphql.ts
+++ src/types/graphql/graphql.ts
@@ -233,14 +233,11 @@ export type ProjectPage = {
 export type Query = {
   __typename?: 'Query';
   findUserByEmail?: Maybe<User>;
-  findProjectsByUser?: Maybe<Array<Maybe<Project>>>;
   /** Find a document from the collection of 'User' by its id. */
   findUserByID?: Maybe<User>;
-  userByEmail?: Maybe<User>;
-  findProjectsByUserEmail?: Maybe<Array<Maybe<Project>>>;
   /** Find a document from the collection of 'Canvas' by its id. */
   findCanvasByID?: Maybe<Canvas>;
-  findProjectsByUserId?: Maybe<Array<Maybe<Project>>>;
+  findProjectsByUserId?: Maybe<Array<Project>>;
   /** Find a document from the collection of 'Project' by its id. */
   findProjectByID?: Maybe<Project>;
 };
@@ -251,23 +248,8 @@ export type QueryFindUserByEmailArgs = {
 };
 
 
-export type QueryFindProjectsByUserArgs = {
-  ref: Scalars['ID'];
-};
-
-
 export type QueryFindUserByIdArgs = {
   id: Scalars['ID'];
-};
-
-
-export type QueryUserByEmailArgs = {
-  email: Scalars['String'];
-};
-
-
-export type QueryFindProjectsByUserEmailArgs = {
-  email: Scalars['String'];
 };
 
 
